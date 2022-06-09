3a97617510137698656d2840ef559f22b03ec448
ignacioandreozzi <ignacio.andreozzi@gmail.com>
2022-06-09 18:10:18 -0300

    procfile

--- app.js
+++ app.js
@@ -6,4 +6,6 @@ app.use(express.static('./public'));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './views/index.html')));
 app.get('/register', (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html')));
 app.get('/login', (req, res) => res.sendFile(path.resolve(__dirname, './views/login.html')));
-app.listen(3000, () => console.log('Corriendo el puerto 3000'));
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo en el puerto 3000");
+})
